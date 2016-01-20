31c37d3aa7439f68d7c36c78099c542d9a6da4e1
craiglandis <craiglandis@hotmail.com>
2016-01-19 22:17:03 -0800

    Create Get-AzureRmVMImages.ps1

---
+++ Get-AzureRmVMImages.ps1
@@ -0,0 +1,72 @@
+param ( $location = 'westus' )
+
+$start = Get-Date
+
+$global:images = @()
+
+$publishers = ( Get-AzureRmVMImagePublisher -Location $location ).PublisherName
+
+$publishers | foreach {
+	
+	$publisher = $_
+	$offers = ( Get-AzureRmVMImageOffer -Location $location -PublisherName $publisher ).Offer
+
+	if (($offers | measure ).count -gt 0)
+	{
+		$offers | foreach {
+
+			$offer = $_
+			$skus = (Get-AzureRmVMImageSku -Location $location -PublisherName $publisher -Offer $offer).Skus
+
+			if (($skus | measure ).count -gt 0)
+			{
+				$skus | foreach {
+
+					$sku = $_
+					$versions = (get-azurermvmimage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku).Version
+
+					if (($versions | measure ).count -gt 0)
+					{
+						$versions | foreach {
+
+							$version = $_
+							#Get-AzureRmvmimage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku -Version $version
+
+							#"Publisher: $publisher Offer: $offer Sku: $sku Version: $version"
+														
+							$image = [pscustomobject]@{
+								Publisher = $publisher
+								Offer     = $offer
+								Sku       = $sku
+								Version   = $version
+								Command   = "Get-AzureRmVMImage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku -Version $version"
+							}
+							"Publisher: $($image.publisher) Offer: $($image.offer) Sku: $($image.sku) Version: $($image.version)"
+							$global:images += $image
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+if (get-command Export-Excel -ErrorAction SilentlyContinue)
+{
+	$output = '.\images.xlsx'
+	$global:images | Export-Excel $output
+}
+else
+{
+	$output = '.\images.csv'
+	$global:images | Export-Csv -path $output -NoTypeInformation	
+}
+
+if (Test-Path $output)
+{
+	"Output: $output"
+}
+
+$end = Get-Date
+$duration = New-Timespan -Start $start -End $end
+Write-Host ('Duration: ' +  ('{0:hh}:{0:mm}:{0:ss}.{0:ff}' -f $duration)) -color Cyan
