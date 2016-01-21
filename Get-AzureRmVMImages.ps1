73d33ff6b36f6e6ba8a49e861e978299d605b9ce
craiglandis <craiglandis@hotmail.com>
2016-01-21 15:41:18 -0800

    Update Get-AzureRmVMImages.ps1

--- Get-AzureRmVMImages.ps1
+++ Get-AzureRmVMImages.ps1
@@ -4,9 +4,9 @@ $start = Get-Date
 
 Login-AzureRmAccount
 
-$images = @()
+$images = New-Object System.Collections.ArrayList
 
-$publishers = ( Get-AzureRmVMImagePublisher -Location $location ).PublisherName
+$publishers = ( Get-AzureRmVMImagePublisher -Location $location ).PublisherName # | select -first 5
 
 $publishers | foreach {
 	
@@ -32,16 +32,9 @@ $publishers | foreach {
 						$versions | foreach {
 
 							$version = $_
-
-							$image = [pscustomobject]@{
-								Publisher = $publisher
-								Offer     = $offer
-								Sku       = $sku
-								Version   = $version
-								Command   = "Get-AzureRmVMImage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku -Version $version"
-							}
-							"Publisher: $($image.publisher) Offer: $($image.offer) Sku: $($image.sku) Version: $($image.version)"
-							$images += $image
+							$image = Get-AzureRmVMImage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku -Version $version							
+							[void]$images.Add($image)
+							"PublisherName: $($image.PublisherName) Offer: $($image.offer) Sku: $($image.sku) Version: $($image.version)"
 						}
 					}
 				}
@@ -50,23 +43,65 @@ $publishers | foreach {
 	}
 }
 
+# Export-Excel is a cmdlet from the Import-Excel module - https://github.com/dfinke/ImportExcel
 if (get-command Export-Excel -ErrorAction SilentlyContinue)
 {
 	$output = '.\images.xlsx'
-	$images | Export-Excel $output
+	$images | select PublisherName,Offer,Skus,Version,Name,@{Name='OSDiskImage';Expression={$_.OSDiskImage | convertto-json}},@{Name='PurchasePlan';Expression={$_.PurchasePlan | convertto-json}},@{Name='DataDiskImages';Expression={$_.DataDiskImages | convertto-json}},Id,Location,FilterExpression | Export-Excel $output
 }
 else
 {
 	$output = '.\images.csv'
-	$images | Export-Csv -path $output -NoTypeInformation	
+	$images | select PublisherName,Offer,Skus,Version,Name,@{Name='OSDiskImage';Expression={$_.OSDiskImage | convertto-json}},@{Name='PurchasePlan';Expression={$_.PurchasePlan | convertto-json}},@{Name='DataDiskImages';Expression={$_.DataDiskImages | convertto-json}},Id,Location,FilterExpression | Export-Csv -path $output -NoTypeInformation
 }
 
 if (Test-Path $output)
 {
-	"Output: $output"
-	"Images: $($images.count)"
+	$numImagesByPublishers = New-Object System.Collections.ArrayList
+
+	$images.publishername | sort -Unique | foreach {
+		$publisher = $_
+		$numPublisherImages = ($images | where {$_.PublisherName -eq $publisher}).count
+		$numMarketplacePublisherImages = ($images | where {$_.PurchasePlan -ne $null -and $_.PublisherName -eq $publisher}).count
+		$numPlatformPublisherImages = ($images | where {$_.PurchasePlan -eq $null -and $_.PublisherName -eq $publisher}).count
+		$numImagesByPublisher = [pscustomobject]@{
+			PublisherName = $publisher
+			TotalImages = $numPublisherImages
+			MarketplaceImages = $numMarketplacePublisherImages
+			PlatformImages    = $numPlatformPublisherImages
+		}
+		[void]$numImagesByPublishers.Add($numImagesByPublisher)
+	}
+	
+	$numPublishers               = $publishers.count 
+	$numMarketplaceImages        = ($images | where {$_.PurchasePlan -ne $null}).count
+	$numMarketplaceImagesLinux   = ($images | where {$_.PurchasePlan -ne $null -and $_.OSDiskImage.OperatingSystem -eq 'Linux'}).count
+	$numMarketplaceImagesWindows = ($images | where {$_.PurchasePlan -ne $null -and $_.OSDiskImage.OperatingSystem -eq 'Windows'}).count
+	$numPlatformImages           = ($images | where {$_.PurchasePlan -eq $null}).count
+	$numPlatformImagesLinux      = ($images | where {$_.PurchasePlan -eq $null -and $_.OSDiskImage.OperatingSystem -eq 'Linux'}).count
+	$numPlatformImagesWindows    = ($images | where {$_.PurchasePlan -eq $null -and $_.OSDiskImage.OperatingSystem -eq 'Windows'}).count
+	$numTotalImages              = $images.count
+	$numTotalLinuxImages         = ($images | where {$_.OSDiskImage.OperatingSystem -eq 'Linux'}).count
+	$numTotalWindowsImages       = ($images | where {$_.OSDiskImage.OperatingSystem -eq 'Windows'}).count
+
+	"Output: $output`n"
+
+	"Total Images ................... $numTotalImages"
+	"  Total Linux Images ........... $numTotalLinuxImages"
+	"  Total Windows Images ......... $numTotalWindowsImages"
+	"Marketplace Images ............. $numMarketplaceImages"	
+	"  Linux Marketplace Images ..... $numMarketplaceImagesLinux"
+	"  Windows Marketplace Images ... $numMarketplaceImagesWindows"
+	"Platform Images ................ $numPlatformImages"
+	"  Linux Platform Images ........ $numPlatformImagesLinux"
+	"  Windows Platform Images ...... $numPlatformImagesWindows"
+
+	"`nTotal Publishers: $numPublishers"
+
+	$numImagesByPublishers | sort TotalImages -Descending | Format-Table -AutoSize
 }
 
 $end = Get-Date
 $duration = New-Timespan -Start $start -End $end
-Write-Host ('Duration: ' +  ('{0:hh}:{0:mm}:{0:ss}.{0:ff}' -f $duration))
+Write-Host ('Script Duration: ' +  ('{0:hh}:{0:mm}:{0:ss}.{0:ff}' -f $duration))
+$global:images = $images
