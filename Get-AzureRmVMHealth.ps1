5ba1778009186f440bbac6ec775bdadd9e4ff1d7
craiglandis <craiglandis@users.noreply.github.com>
2017-08-08 10:36:02 -0700

    Update Get-AzureRmVMHealth.ps1

--- Get-AzureRmVMHealth.ps1
+++ Get-AzureRmVMHealth.ps1
@@ -1,50 +1,78 @@
 param(
-    $resourceGroupName,
-    $name
+    [string]$resourceGroupName,
+    [string]$name,
+    [string]$path,
+    [switch]$openJsonFile = $true
 )
 
-$vm = get-azurermvm -ResourceGroupName $resourceGroupName -Name $name -ErrorAction Stop
-$vmstatus = $vm | get-azurermvm -status -ErrorAction Stop
-
-
-if ($vm.DiagnosticsProfile.Bootdiagnostics.Enabled)
-{
-    if ($vmstatus.bootdiagnostics.ConsoleScreenshotBlobUri)
+function Get-JsonFromSerialLog ($serialLogFilePath)
+{    
+    $serialLogFileName = split-path -Path $serialLogFilePath -Leaf
+    $jsonFileName = "$($serialLogFileName.SubString(0,$serialLogFileName.Length-4)).json"
+    $jsonFilePath = "$env:TEMP\$jsonFileName"
+    $serialLog = get-content -Path $serialLogFilePath -Raw
+    for($i=0; $i -le ($serialLog.count); $i++){if($serialLog[$i] -match 'Microsoft Azure VM Health Report - Start'){$jsonString = $serialLog[$i+1]}}
+    $json = $jsonString | ConvertFrom-Json
+    if ($json){$json | ConvertTo-Json | out-file $jsonFilePath}
+    if (test-path $jsonFilePath)
     {
-        $consoleScreenshotBlobUri = $vmstatus.bootdiagnostics.ConsoleScreenshotBlobUri
+        get-content $jsonFilePath
+        "VM Health JSON: $jsonFilePath"
+        if ($openJsonFile)
+        {
+            invoke-item $jsonFilePath
+        }
+    }
+}
+
+if ($resourceGroupName -and $name)
+{
+    $vm = get-azurermvm -ResourceGroupName $resourceGroupName -Name $name -ErrorAction Stop
+    $vmstatus = $vm | get-azurermvm -status -ErrorAction Stop
+
+    if ($vm.DiagnosticsProfile.Bootdiagnostics.Enabled)
+    {
+        if ($vmstatus.bootdiagnostics.ConsoleScreenshotBlobUri)
+        {
+            $consoleScreenshotBlobUri = $vmstatus.bootdiagnostics.ConsoleScreenshotBlobUri
+        }
+        else
+        {
+            "ConsoleScreenshotBlobUri property not populated"
+            exit
+        }
     }
     else
     {
-        "ConsoleScreenshotBlobUri property not populated"
+        "Bootdiagnostics: $($vm.DiagnosticsProfile.Bootdiagnostics.Enabled)"
+        exit
+    }
+
+    $storageAccountName = $consoleScreenshotBlobUri.split('/')[2].split('.')[0]
+    $storageContainer = $consoleScreenshotBlobUri.split('/')[3]
+    #TODO If boot diag storage account can reside in a different RG than the VM's RG, need a different way to get the RG of the boot diag storage account
+    $storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName)[0].Value
+    $blobs = get-azurestorageblob -Container $storageContainer -Context $storageContext
+    $log = $blobs | where {$_.Name.EndsWith('.serialconsole.log')} | select -first 1
+    $log | Get-AzureStorageBlobContent -Destination $env:TEMP -Force | Out-Null
+    $logFilePath = "$env:TEMP\$($log.Name)"
+    Get-JsonFromSerialLog $logFilePath
+    "`nserial log:     $logFilePath"    
+}
+elseif ($path)
+{
+    if (test-path $path)
+    {
+        Get-JsonFromSerialLog $path
+    }
+    else 
+    {
+        Write-Error "File not found: $path"    
         exit
     }
 }
-else
+else 
 {
-    "Bootdiagnostics: $($vm.DiagnosticsProfile.Bootdiagnostics.Enabled)"
+    write-error "Use -resourceGroupName and -name to download the log, or -path to parse output from log already downloaded."
     exit
-}
-
-$storageAccountName = $consoleScreenshotBlobUri.split('/')[2].split('.')[0]
-$storageContainer = $consoleScreenshotBlobUri.split('/')[3]
-#TODO If boot diag storage account can reside in a different RG than the VM's RG, need a different way to get the RG of the boot diag storage account
-$storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName)[0].Value
-$blobs = get-azurestorageblob -Container $storageContainer -Context $storageContext
-$logs = $blobs | where {$_.Name.EndsWith('.serialconsole.log')}
-$logs | Get-AzureStorageBlobContent -Force | Out-Null
-$logs | foreach {
-    $logFileName = $_.name
-    $logFilePath = "$pwd\$logFileName"
-    $jsonFileName = "$($logFileName.SubString(0,$logFileName.Length-4)).json"
-    $jsonFilePath = "$pwd\$jsonFileName"
-    $log = get-content $logFilePath
-    for($i=0; $i -le ($log.count); $i++){if($log[$i] -match 'Microsoft Azure VM Health Report - Start'){$jsonString = $log[$i+1]}}  
-    $json = $jsonString | ConvertFrom-Json
-    if ($json){$json | ConvertTo-Json | out-file $jsonFilePath}
-    get-content $jsonFilePath
-    "`nserial log:     $logFilePath"
-    "vm health json: $jsonFilePath"
-    "logFilePath: $logFilePath"
-    "Boot diag storage account: $storageAccountName"
-    "VMAgentVersion: $($vmstatus.vmagent.VmAgentVersion)"
 }
