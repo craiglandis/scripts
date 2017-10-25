eaacc38500e73ec745cf00cf46bfc7c02b14b4dd
craiglandis <craiglandis@users.noreply.github.com>
2017-10-25 14:31:30 -0700

    Create New-VHDUpload.ps1

---
+++ New-VHDUpload.ps1
@@ -0,0 +1,45 @@
+param(
+    $resourceGroupName = 'clwestcentralusrg1',
+    $storageAccountName = 'clwestcentralussa1',
+    $location = 'westcentralus',
+    $containerName = 'images',
+    $localFilePath = "\\ws\images$\ws2003-4GBaligned.vhd",
+    $skuName ='Standard_LRS'
+)
+
+# Create resource group if it doesn't exit
+if(!(Get-AzureRmResourceGroup -Name $resourceGroupName -Location $location -ErrorAction SilentlyContinue))
+{
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+}
+
+# Create storage account if it doesn't exit
+if(!(Get-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue))
+{
+    $storageAccount = New-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -SkuName $skuName -Location $location
+}
+
+$storageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName)[0].Value
+$storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
+
+# Create contain if it doesn't exist
+if(!(Get-AzureStorageContainer -Context $storageContext -Name $containerName -ErrorAction SilentlyContinue))
+{
+    New-AzureStorageContainer -Context $storageContext -Name $containerName
+}
+
+if ($storageAccount)
+{
+    $destination = "$($storageAccount.PrimaryEndpoints.Blob)$containerName/$(split-path $localFilePath -leaf)"
+}
+else 
+{
+    Write-Host "Make sure storage account exists: $storageAccountName"    
+    exit
+}
+
+# Upload VHD
+$command = "Add-AzureRmVhd -ResourceGroupName $resourceGroupName -Destination $destination -LocalFilePath $localFilePath"
+write-host $command 
+Invoke-Expression $command
+#Add-AzureRmVhd -ResourceGroupName $resourceGroupName -Destination $destination -LocalFilePath $localFilePath
