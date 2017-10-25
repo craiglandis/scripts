35c2ace3ef58b86f237aab0512bc40aac66b4962
craiglandis <craiglandis@users.noreply.github.com>
2017-10-25 15:03:39 -0700

    Update New-VHDUpload.ps1

--- New-VHDUpload.ps1
+++ New-VHDUpload.ps1
@@ -10,35 +10,59 @@ param(
 # Create resource group if it doesn't exit
 if(!(Get-AzureRmResourceGroup -Name $resourceGroupName -Location $location -ErrorAction SilentlyContinue))
 {
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+    $resourceGroup = New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
 }
 
 # Create storage account if it doesn't exit
-if(!(Get-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue))
+if ($resourceGroup)
 {
-    $storageAccount = New-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -SkuName $skuName -Location $location
+    if(!(Get-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue))
+    {
+        $storageAccount = New-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -SkuName $skuName -Location $location
+    }
 }
-
-$storageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName)[0].Value
-$storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
-
-# Create contain if it doesn't exist
-if(!(Get-AzureStorageContainer -Context $storageContext -Name $containerName -ErrorAction SilentlyContinue))
-{
-    New-AzureStorageContainer -Context $storageContext -Name $containerName
-}
-
-if ($storageAccount)
-{
-    $destination = "$($storageAccount.PrimaryEndpoints.Blob)$containerName/$(split-path $localFilePath -leaf)"
-}
-else 
+else
 {
     Write-Host "Make sure storage account exists: $storageAccountName"    
     exit
 }
 
+if ($storageAccount)
+{
+    $storageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName)[0].Value
+    $storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
+
+    # Create container if it doesn't exist
+    if(!(Get-AzureStorageContainer -Context $storageContext -Name $containerName -ErrorAction SilentlyContinue))
+    {
+        $container = New-AzureStorageContainer -Context $storageContext -Name $containerName
+    }    
+}
+else
+{
+    Write-Host "Make sure storage account exists: $storageAccountName"    
+    exit
+}
+
+if (test-path $localFilePath)
+{
+    $destination = "$($storageAccount.PrimaryEndpoints.Blob)$containerName/$(split-path $localFilePath -leaf)"
+}
+else
+{
+    Write-Host "File not found: $localFilePath"
+    exit
+}
+
 # Upload VHD
-$command = "Add-AzureRmVhd -ResourceGroupName $resourceGroupName -Destination $destination -LocalFilePath $localFilePath"
-write-host $command 
-Invoke-Expression $command
+if ($container)
+{
+    $command = "Add-AzureRmVhd -ResourceGroupName $resourceGroupName -Destination $destination -LocalFilePath $localFilePath"
+    write-host $command
+    Invoke-Expression $command
+}
+else
+{
+    Write-Host "Container not found: $containerName"
+    exit
+}
