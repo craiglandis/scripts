a12b738d6573a43c544ac4ba592f13d8f896b0f2
craiglandis <craiglandis@users.noreply.github.com>
2018-06-09 18:07:37 -0700

    Update invoke-sysprep.ps1

--- invoke-sysprep.ps1
+++ invoke-sysprep.ps1
@@ -1,4 +1,5 @@
 $taskName = 'sysprep'
 $action = New-ScheduledTaskAction -execute 'c:\windows\system32\sysprep\sysprep.exe' -argument '/generalize /oobe /shutdown /mode:vm'
 register-scheduledtask -action $action -taskname $taskName -force
+start-sleep -seconds 60
 start-scheduledtask -TaskName $taskName
