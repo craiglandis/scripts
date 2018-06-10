f679aa2a148b8360af5aa2085bb889f32f6e30b1
craiglandis <craiglandis@users.noreply.github.com>
2018-06-09 20:39:00 -0700

    Update invoke-sysprep.ps1

--- invoke-sysprep.ps1
+++ invoke-sysprep.ps1
@@ -1,5 +1,9 @@
+$path = "c:\scripts"
+$filePath = "$scriptPath\invoke-sysprep.ps1"
+new-item -Path $path -ItemType Directory
+'start-sleep -seconds 300' | out-file -filepath $filePath -append
+'c:\windows\system32\sysprep\sysprep.exe /generalize /oobe /shutdown /mode:vm' | out-file -filepath $filePath -append
 $taskName = 'sysprep'
-$action = New-ScheduledTaskAction -execute 'c:\windows\system32\sysprep\sysprep.exe' -argument '/generalize /oobe /shutdown /mode:vm'
+$action = New-ScheduledTaskAction -execute 'powershell.exe' -argument "-executionpolicy bypass -file $filePath"
 register-scheduledtask -action $action -taskname $taskName -user system -force
-start-sleep -seconds 300
 start-scheduledtask -TaskName $taskName
