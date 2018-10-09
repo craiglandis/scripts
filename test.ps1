74e78a9d7ed39ac249a0665c946a4c4cad727477
craiglandis <craiglandis@users.noreply.github.com>
2018-10-09 09:30:26 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
 $batchFile = "$env:allusersprofile\Microsoft\Windows\Start Menu\Programs\StartUp\RunHyperVManagerAndVMConnect.cmd"
 "start $env:windir\System32\mmc.exe $env:windir\System32\virtmgmt.msc" | out-file -FilePath $batchFile -Append -Encoding Default
 "start $env:windir\System32\vmconnect.exe localhost $nestedGuestVmName" | out-file -FilePath $batchFile -Append -Encoding Default
+get-content $batchFile
