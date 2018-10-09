d6ccef56acb52bde66a0b5e30b30aa92ae092815
craiglandis <craiglandis@users.noreply.github.com>
2018-10-09 09:57:34 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
+$newItem = New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value 1 –force
 $batchFile = "$env:allusersprofile\Microsoft\Windows\Start Menu\Programs\StartUp\RunHyperVManagerAndVMConnect.cmd"
 "start $env:windir\System32\mmc.exe $env:windir\System32\virtmgmt.msc" | out-file -FilePath $batchFile -Append -Encoding Default
 "start $env:windir\System32\vmconnect.exe localhost $nestedGuestVmName" | out-file -FilePath $batchFile -Append -Encoding Default
