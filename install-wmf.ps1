6452d607185889c4db796b1cade7389f80f914b7
craiglandis <craiglandis@users.noreply.github.com>
2019-06-04 21:43:19 -0700

    Update install-wmf.ps1

--- install-wmf.ps1
+++ install-wmf.ps1
@@ -1,16 +1,59 @@
 #
 #(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/craiglandis/scripts/master/install-wmf.ps1', "$env:temp\install-wmf.ps1"); set-executionpolicy unrestricted -force; invoke-expression -command "$env:temp\install-wmf.ps1"
 
+function out-log()
+{
+    param(
+        [string]$text,
+        [string]$prefix = 'timespan'
+    )
 
+    if ($prefix -eq 'timespan' -and $startTime)
+    {
+        $timespan = new-timespan -Start $startTime -End (get-date)
+        $timespanString = '[{0:mm}:{0:ss}]' -f $timespan
+        write-host $timespanString -nonewline -ForegroundColor White
+        write-host " $text"
+        (($timespanString + " $text") | out-string).Trim() | out-file $logFile -Append
+    }
+    elseif ($prefix -eq 'both' -and $startTime)
+    {
+        $timestamp = get-date -format "yyyy-MM-dd hh:mm:ss"
+        $timespan = new-timespan -Start $startTime -End (get-date)
+        $timespanString = "$($timestamp) $('[{0:mm}:{0:ss}]' -f $timespan)"
+        write-host $timespanString -nonewline -ForegroundColor White
+        write-host " $text"
+        (($timespanString + " $text") | out-string).Trim() | out-file $logFile -Append
+    }
+    else
+    {
+        $timestamp = get-date -format "yyyy-MM-dd hh:mm:ss"
+        write-host $timestamp -nonewline -ForegroundColor Cyan
+        write-host " $text"
+        (($timestamp + $text) | out-string).Trim() | out-file $logFile -Append
+    }
+}
+
+set-strictmode -version Latest
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+$PSDefaultParameterValues['*:WarningAction'] = 'SilentlyContinue'
+
+$startTime = get-date
+$timestamp = get-date $startTime -format yyyyMMddhhmmss
+$scriptPath = split-path -path $MyInvocation.MyCommand.Path
+$scriptPathParent = split-path -path $MyInvocation.MyCommand.Path -parent
+$scriptName = (split-path -path $MyInvocation.MyCommand.Path -leaf).Split('.')[0]
+$logFile = "$scriptPathParent\$($scriptName)_$($timestamp).log"
+out-log $logFile
 $webClient = New-Object System.Net.WebClient
 
 if ((Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release -ge 461814)
 {
-    write-host "NET Framework 4.72 already installed"
+    out-log "NET Framework 4.72 already installed"
 }
 else
 {
-    write-host "Installing NET Framework 4.72"
+    out-log "Installing NET Framework 4.72"
     $url = 'https://download.microsoft.com/download/6/E/4/6E48E8AB-DC00-419E-9704-06DD46E5F81D/NDP472-KB4054530-x86-x64-AllOS-ENU.exe'
     $fileName = $url -split '/' | select -last 1
     $filePath = "$env:TEMP\$fileName"
@@ -18,19 +61,19 @@ else
     invoke-expression -command "$filePath /q /norestart"
 
     do {
-        write-host "Waiting for NET Framework 4.72 install to complete"
+        out-log "Waiting for NET Framework 4.72 install to complete"
         start-sleep 5
     } until ((Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release -ge 461814)
-    write-host "NET Framework 4.72 install completed"
+    out-log "NET Framework 4.72 install completed"
 }
 
-if (get-wmiobject -Query "Select HotFixID from Win32_QuickFixEngineering where HotFixID='KB3191566'")
+if ($host.version -ge 5.1)
 {
-    write-host "WMF 5.1 already installed"
+    out-log "Already using Powershell 5.1+"
 }
 else
 {
-    write-host "Installing WMF 5.1"
+    out-log "Installing WMF 5.1"
     $url = 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7AndW2K8R2-KB3191566-x64.zip'
     $fileName = $url -split '/' | select -last 1
     $filePath = "$env:TEMP\$fileName"
@@ -59,15 +102,51 @@ else
     do {
         start-sleep 5
     } until (get-wmiobject -Query "Select HotFixID from Win32_QuickFixEngineering where HotFixID='KB3191566'")
-    write-host "restart-computer -force"
+    out-log "Creating onstart scheduled task to run script again at startup:"
+    out-log $scriptPath
+    schtasks /create /tn bootstrap /sc onstart /delay 0000:30 /rl highest /ru system /tr "powershell.exe -file $scriptPath"
+    if ($?)
+    {
+        out-log "Restarting to complete WMF 5.1 install"
+        restart-computer -Force
+    }
 }
 
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\StreamProvider -Name LastFullPayloadTime -Value 0 -PropertyType DWord -Force
-install-packageprovider -name NuGet -MinimumVersion 2.8.5.201 -Force
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-install-module -name PSWindowsUpdate -AllowClobber -Force
+if (get-wmiobject -Query "Select HotFixID from Win32_QuickFixEngineering where HotFixID='KB3191566'")
+{
+    out-log "Setting LastFullPayloadTime reg value to workaround WMF 5.1 sysprep issue"
+    New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\StreamProvider -Name LastFullPayloadTime -Value 0 -PropertyType DWord -Force
+}
 
-#schtasks /create /tn install /tr c:\apps\myapp.exe /sc onstart
+if ([version](get-packageprovider nuget).Version -ge [version]'2.8.5.201')
+{
+    out-log "NuGet already installed"
+}
+else
+{
+    out-log "Installing NuGet"
+    install-packageprovider -name NuGet -MinimumVersion 2.8.5.201 -Force
+}
+
+if ((Get-PSRepository -Name PSGallery).InstallationPolicy -eq 'Trusted')
+{
+    out-log "PSGallery installation policy is already Trusted"
+}
+else
+{
+    out-log "Setting PSGallery installation policy to Trusted"
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+}
+
+if (get-module -name PSWindowsUpdate -ListAvailable)
+{
+    out-log "PSWindowsUpdate module already installed"
+}
+else
+{
+    out-log "Installing PSWindowsUpdate module"
+    install-module -name PSWindowsUpdate -Force
+}
 
 <#
 get-windowsupdate -Install -AcceptAll -AutoReboot -IgnoreUserInput -RecurseCycle 5 -verbose
