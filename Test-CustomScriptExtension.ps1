c55b9f2363ce0f66daaaf8b8eff51936b1c07f31
craiglandis <craiglandis@users.noreply.github.com>
2020-11-29 18:38:00 -0800

    Update Test-CustomScriptExtension.ps1

--- Test-CustomScriptExtension.ps1
+++ Test-CustomScriptExtension.ps1
@@ -1,27 +1,5 @@
-# Set-AzVMCustomScriptExtension -Location 'westus2' -ResourceGroupName 'rg1' -VMName 'vm1' -Name 'CustomScriptExtension' -FileUri 'https://raw.githubusercontent.com/craiglandis/scripts/master/Test-CustomScriptExtension.ps1' -Run 'Test-CustomScriptExtension.ps1'
 Set-PSDebug -Trace 2
 Start-Transcript
-
-<#
-$enableScriptBlockLoggingValue = Get-ItemPropertyValue -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' -Name "EnableScriptBlockLogging" -ErrorAction SilentlyContinue
-if ($enableScriptBlockLoggingValue)
-{
-    Write-Output "EnableScriptBlockLoggingValue: $enableScriptBlockLoggingValue"
-}
-else
-{
-    Write-Output "EnableScriptBlockLoggingValue registry entry does not exist"
-    $scriptBlockLoggingKey = Get-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' -ErrorAction SilentlyContinue
-    if ((Test-Path -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging') -eq $false)
-    {
-        # Create ScriptBlockLogging key if it does not exist
-        New-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' -Force
-    }
-    New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' -Name "EnableScriptBlockLogging" -PropertyType 'DWord' -Value 1 -Force
-    New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' -Name "EnableScriptBlockInvocationLogging" -PropertyType 'DWord' -Value 1 -Force
-}
-#>
-
 $scriptName = $MyInvocation.MyCommand.Name
 "'no cmdlet' output from $scriptName"
 Write-Debug "Write-Debug output from $scriptName"
@@ -32,18 +10,6 @@ Write-Output "Write-Output output from $scriptName"
 Write-Progress "Write-Progress output from $scriptName"
 Write-Warning "Write-Warning output from $scriptName"
 Write-Verbose "Write-Verbose output from $scriptName"
-
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/craiglandis/scripts/master/Test-CustomScriptExtensionChild.ps1'))
-
 exit 1
 Stop-Transcript
-Set-PSDebug -Trace 0
-
-<#
-New-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' -Force
-New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' -Name "EnableScriptBlockLogging" -PropertyType 'DWord' -Value 1 -Force
-New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' -Name "EnableScriptBlockLogging" -PropertyType 'DWord' -Value 0 -Force
-Get-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' #-ErrorAction SilentlyContinue
-Get-ItemPropertyValue -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' -Name "EnableScriptBlockLogging" -ErrorAction SilentlyContinue
-Remove-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging' -Name "EnableScriptBlockLogging"
-#>
